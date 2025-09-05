9af9d1f1d17a69cd2f4a2886d62086faf3f1ee1b
Mikolaj0524 <96992608+Mikolaj0524@users.noreply.github.com>
2025-09-06 01:12:09 +0200

    init commit

---
+++ src/main/java/fun/mikolaj0524/pillars/Pillars.java
@@ -0,0 +1,17 @@
+package fun.mikolaj0524.pillars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pillars extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
